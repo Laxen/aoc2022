b1f769f3781cf8fd5954f21e36ed69e7bda547c5
Alexander Ganslandt <alex.ganslandt@gmail.com>
2022-12-02 20:19:31 +0100

    Update template

--- template/main.py
+++ template/main.py
@@ -6,11 +6,11 @@ def make_data(input_file):
         pass
 
 if len(sys.argv) > 1:
-    print("EXAMPLE\n")
-    data = make_data("example")
-else:
     print("INPUT\n")
     data = make_data("input")
+else:
+    print("EXAMPLE\n")
+    data = make_data("example")
 
 # -----------------
 
