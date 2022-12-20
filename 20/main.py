125126c0d6ba26343df8ece9570b7aa057330315
Alexander Ganslandt <alex.ganslandt@gmail.com>
2022-12-20 06:32:53 +0100

    Day 20 P1: Shuffling numbers
    Super easy. I think storing each number as an object and then keeping
    references to those objects in a list that is ordered according to how
    the numbers should be processed helped a lot. Otherwise the math looked
    kinda tough.

---
+++ 20/main.py
@@ -0,0 +1,62 @@
+import sys
+from copy import deepcopy
+from pyhelpers import Parser
+
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return str(self.value)
+
+def make_data(input_file):
+    res = []
+    with open(input_file, "r") as f:
+        for line in f:
+            num = Number(int(line.rstrip()))
+            res.append(num)
+    return res
+
+if len(sys.argv) > 1:
+    print("INPUT\n")
+    data = make_data("input")
+else:
+    print("EXAMPLE\n")
+    data = make_data("example")
+
+# -----------------
+
+order = data.copy()
+
+for n in order:
+    i = data.index(n)
+    # print(i, n)
+    data.remove(n)
+
+    new_i = (i + n.value)%len(data)
+    if new_i == 0:
+        new_i = len(data)
+
+    data.insert(new_i, n)
+    # print(data)
+
+i_zero = 0
+for i, n in enumerate(data):
+    if n.value == 0:
+        i_zero = i
+        break
+
+s = 0
+for i in range(1000):
+    new_i = (i_zero + i) % len(data)
+s += data[new_i+1].value
+
+for i in range(2000):
+    new_i = (i_zero + i) % len(data)
+s += data[new_i+1].value
+
+for i in range(3000):
+    new_i = (i_zero + i) % len(data)
+s += data[new_i+1].value
+
+print(s)
